da006cf04924e5e5b71ce3a9b25120ad878fb06f
Salahuddin M. ElKazak <salahuddin.kazak@gmail.com>
2017-02-10 18:41:32 +0200

    Fix motion sequence

--- js/gameengine.js
+++ js/gameengine.js
@@ -4,13 +4,17 @@ var myGamePieceAnim=['img/char1.png'
 		,'img/char3.png'
 		,'img/char4.png'
 		,'img/char5.png'
+		,'img/char6.png'
+		,'img/char7.png'
+		,'img/char8.png'
+		,'img/char9.png'
 		];
 var myGamePieceAnimIdx=0;
 var myBackground;
 var change=false;
 
 function startGame() {
-    myGamePiece = new component(61, 134, myGamePieceAnim[myGamePieceAnimIdx], 10, 360, "image");
+    myGamePiece = new component(76, 134, myGamePieceAnim[myGamePieceAnimIdx], 10, 360, "image");
     myBackground = new component(965, 495, "img/bg1.png", 0, 0, "background");
     myGameArea.start();
 }
@@ -72,11 +76,11 @@ function component(width, height, color, x, y, type) {
         this.x += this.speedX;
         this.y += this.speedY;        
         if (this.type == "background") {
-            if (this.x == -(this.width)) {
-                this.x = 0;
+            if (this.x <= -(this.width)) {
+                this.x = this.x+this.width;
             }
-            if (this.x == (this.width)) {
-                this.x = 0;
+            if (this.x >= (this.width)) {
+                this.x = this.x-this.width;
             }
         }
     }
@@ -91,20 +95,22 @@ function updateGameArea() {
 }
 
 function move(dir) {
-    if (dir == "up") {myBackground.speedY = 1/2; 
-		myGamePieceAnimIdx=--myGamePieceAnimIdx%myGamePieceAnim.length;
+    if (dir == "up") {myBackground.speedY = 2; 
+		myGamePieceAnimIdx=++myGamePieceAnimIdx%myGamePieceAnim.length;
 	}
 		
     if (dir == "down") {
-		myBackground.speedY = -1/2; 
+		myBackground.speedY = -2; 
 		myGamePieceAnimIdx=++myGamePieceAnimIdx%myGamePieceAnim.length;
 	}
-    if (dir == "left") {myBackground.speedX = 1/2; 
-		myGamePieceAnimIdx=--myGamePieceAnimIdx%myGamePieceAnim.length;
-	}
-    if (dir == "right") {myBackground.speedX = -1/2; 
+    if (dir == "left") {myBackground.speedX = 2; 
 		myGamePieceAnimIdx=++myGamePieceAnimIdx%myGamePieceAnim.length;
 	}
+    if (dir == "right") {myBackground.speedX = -2; 
+		myGamePieceAnimIdx=++myGamePieceAnimIdx%myGamePieceAnim.length;
+	}
+	updateGameArea();
+	clearmove();
 }
 
 function clearmove() {
@@ -112,4 +118,25 @@ function clearmove() {
     myGamePiece.speedY = 0; 
     myBackground.speedX = 0; 
     myBackground.speedY = 0; 
+}
+
+document.addEventListener("keydown", keyDownTextField, false);
+
+function keyDownTextField(e) {
+	var keyCode = e.keyCode;
+	switch(keyCode)
+	{
+	case 37:
+		move('left');
+		break;
+	case 38:
+		move('up');
+		break;
+	case 39:
+		move('right');
+		break;
+	case 40:
+		move('down');
+		break;
+	}
 }
